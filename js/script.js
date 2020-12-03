201aae2d28eb269267e3fc74f9b65f6155cc45a6
Luca Cavretti <cavretiluca@live.it>
2020-12-03 17:03:34 +0100

    Added Movie/Series selection and search

--- js/script.js
+++ js/script.js
@@ -3,31 +3,48 @@ const API_KEY = '3247cb046b5f50419695d648556acfa9';
 const myApp = new Vue({
     el: '#root',
     data: {
-        loader: false,
+        // loader: false,
         movies: [],
         series: [],
         userSrcInput: "",
+        searchType: "Scegli una tipologia..",
         path: 'https://image.tmdb.org/t/p/w500',
         stars: 2 /* to transform the vote from 1-10 to 1-5 */
     },
     methods: {
-        search: function() {
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                    params: {
-                        'api_key': API_KEY,
-                        query: this.userSrcInput
-                    }
-                })
-                .then(r => this.movies = r.data.results)
+        searchMovies: function() {
+
         },
-        calcStars(n) {
-            return Math.ceil(n / this.stars)
+        searchSeries: function() {
+
         },
-        loaderTimeout() {
-            setTimeout(function() { this.loader = true; }, 3000);
+        calcStars(index, n) {
+            return index <= Math.ceil(n / this.stars) ? 'fas fa-star' : 'far fa-star';
+        },
+        selectType() {
+            if (this.searchType === 'films') {
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+                        params: {
+                            'api_key': API_KEY,
+                            query: this.userSrcInput
+                        }
+                    })
+                    .then(r => this.movies = r.data.results)
+            } else if (this.searchType === 'series') {
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+                        params: {
+                            'api_key': API_KEY,
+                            query: this.userSrcInput
+                        }
+                    })
+                    .then(r => this.movies = r.data.results)
+            }
         }
-    },
-    mounted() {
-        this.loaderTimeout()
+        //     loaderTimeout() {
+        //         setTimeout(function() { this.loader = true; }, 3000);
+        //     }
+        // },
+        // mounted() {
+        //     this.loaderTimeout()
     },
 })
